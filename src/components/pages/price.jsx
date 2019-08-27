df294622de69bb8d570d88ef62031bd1fd99c8df
gh83 <obuhov.gh@gmail.com>
2019-08-27 15:00:33 +0300

    loader, async await price

--- src/components/pages/price.jsx
+++ src/components/pages/price.jsx
@@ -3,55 +3,60 @@ import axios from "axios";
 import { OutTable, ExcelRenderer } from "react-excel-renderer";
 
 import "./price.less";
+import Loader from '../ui/loader';
 
 export default class Price extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            loading: true,
             cols: undefined,
             rows: undefined
         };
-    }
+    };
 
-    componentDidMount() {
-        axios({
-            method: "get",
-            url: "/price/price-frezer.xlsx",
-            responseType: "blob",
-            data: {}
-        })
-            .then(({ data }) => {
-                ExcelRenderer(data, (err, resp) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        this.setState({
-                            cols: resp.cols,
-                            rows: resp.rows
-                        });
-                        console.log(this.state.cols,'---',this.state.rows);
-                    }
-                });
-            })
-            .catch(error => console.log(error));
+    // componentDidMount() {
+    //     axios({
+    //         method: "get",
+    //         url: "/price/price-frezer.xlsx",
+    //         responseType: "blob"
+    //     })
+    //         .then(({ data }) => {
+    //             ExcelRenderer(data, (err, resp) => {
+    //                 if (err) { console.log(err); }
+    //                 else { this.setState({ cols: resp.cols, rows: resp.rows }); }
+    //             });
+    //         })
+    //         .catch(error => console.log(error));
+    // };
+    async componentDidMount() {
+        try {
+            const response = await axios({ method: "get", url: "/price/price-frezer.xlsx", responseType: "blob" });
+            ExcelRenderer(response.data, (err, resp) => {
+                if (err) { console.log(err); }
+                else { this.setState({ cols: resp.cols, rows: resp.rows, loading: false }); }
+            });
+        }
+        catch (error) { console.log(error) }
     };
 
     render() {
         return (
             <div className="price">
                 <a href="/price/price-frezer.xlsx" download>Скачать прайс лист</a>
-                <div className='price__table'>
-                    {this.state.cols !== undefined && this.state.rows !== undefined ? (
-                        <OutTable
-                            data={this.state.rows}
-                            columns={this.state.cols}
-                            tableClassName="ExcelTable2007"
-                            tableHeaderRowClass="heading"
-                        />
-                    ) : null}
-                </div>
+                {this.state.loading
+                    ? <Loader />
+                    : (<div className='price__table'>
+                        {this.state.cols !== undefined && this.state.rows !== undefined
+                            ? (<OutTable
+                                data={this.state.rows}
+                                columns={this.state.cols}
+                                tableClassName="ExcelTable2007"
+                                tableHeaderRowClass="heading" />)
+                            : null}
+                    </div>)}
             </div>
         );
     }
-}
+};
---
+++ src/components/ui/loader.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import './loader.less';
+
+export default props => (<div className="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>);
